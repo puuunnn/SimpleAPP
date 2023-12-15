b27ced30b810cbd3ac37c795bce59b806e2c61cd
puuunnn <farhans7d@gmail.com>
2023-12-15 20:56:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/simpleapp/Second_Activity.java
@@ -0,0 +1,18 @@
+package com.example.simpleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class Second_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        WebView mywebview = (WebView) findViewById(R.id.webview);
+        mywebview.loadUrl("https://www.poltekssn.ac.id");
+    }
+}
